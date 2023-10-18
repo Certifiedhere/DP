7b32347a9e604230e7b8fae22cc5573bcb9bb070
Certified Coder <120272679+Certifiedhere@users.noreply.github.com>
2023-10-18 14:07:26 +0530

    Update 05 deep neural network for performing binary.py

--- 05 deep neural network for performing binary.py
+++ 05 deep neural network for performing binary.py
@@ -4,21 +4,6 @@ from keras.layers import Dense
 from keras.models import Sequential
 dataset = loadtxt("C:\\Users\\pima-indians-diabetes.csv", delimiter=',')
 
-import numpy as np
-from keras.layers import Dense
-from keras.models import Sequential
-model = Sequential()
-model.add(Dense(units=2, activation='relu', input_dim=2))
-model.add(Dense(units=1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam',
-metrics=['accuracy'])
-print(model.summary())
-print( model.get_weights())
-X = np.array([[0.,0.], [0.,1.], [1.,0.], [1.,1.]])
-Y = np.array([0.,1.,1.,0.])
-model.fit(X,Y, epochs=1000, batch_size=4)
-print(model.get_weights())
-print(model.predict(X, batch_size=4))
 
 print(dataset)
 X=dataset[:,0:8]
